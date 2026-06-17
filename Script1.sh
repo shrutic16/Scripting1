4f7603936bbf6c308f9131be3c1c415e22d770e2
EC2 Default User <ec2-user@ip-172-31-44-237.ap-south-1.compute.internal>
2026-06-17 13:56:35 +0000

    This is a new commit from EC2

--- Script1.sh
+++ Script1.sh
@@ -3,6 +3,5 @@ echo "We are from batch-33 and learning git/gihub"
 echo "We are learning Branching"
 echo "I am learning Git"
 echo "Hello, How are you?"
-echo"I am from batch-32"
-echo" I am learning gitbash"
+echo"I am from batch-33"
 
